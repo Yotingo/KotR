f43d72694f167992129fdca6b6fe06343848c1f3
vazor <vazor222@gmail.com>
2022-01-30 13:50:44 -0700

    sfx source prefab

---
+++ Assets/Scripts/PlayAudioSFX.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioSFX : MonoBehaviour
+{
+	public AudioSource arrow1;
+	public AudioSource arrow2;
+	public AudioSource type;
+	public AudioSource joinLaugh;
+	public AudioSource joinLaughRare;
+	public AudioSource openClose;
+	
+	public void playArrowSound()
+	{
+		int r = Random.Range(0, 1);
+		if( r > 0 )
+		{
+			arrow1.PlayOneShot(arrow1.clip);
+		}
+		else
+		{
+			arrow2.PlayOneShot(arrow2.clip);
+		}
+	}
+
+	public void playJoinLaugh()
+	{
+		float jr = Random.Range(0, 1);
+		if( jr < .9 )
+		{
+			joinLaugh.PlayOneShot(joinLaugh.clip);
+		}
+		else
+		{
+			joinLaughRare.PlayOneShot(joinLaughRare.clip);
+		}
+	}
+}
